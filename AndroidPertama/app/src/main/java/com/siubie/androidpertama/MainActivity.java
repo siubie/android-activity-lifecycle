4eeffc480f983cf1cd69e637492d32c9b8c477da
siubie <putraprima@gmail.com>
2016-09-10 21:21:28 +0700

    stringmanual

--- AndroidPertama/app/src/main/java/com/siubie/androidpertama/MainActivity.java
+++ AndroidPertama/app/src/main/java/com/siubie/androidpertama/MainActivity.java
@@ -2,7 +2,6 @@ package com.siubie.androidpertama;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,37 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Event On Create",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this,"Event On Start",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this,"Event On Resume",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this,"Event On Pause",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this,"Event On Stop",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this,"Event On Destroy",Toast.LENGTH_SHORT).show();
     }
 
 }
